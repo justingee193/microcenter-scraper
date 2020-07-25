bc13465656349fd1ebea609dac750b1da0df4a70
justingee193 <56926170+justingee193@users.noreply.github.com>
2020-07-24 20:19:28 -0400

    added pipeline.py

---
+++ pipeline.py
@@ -0,0 +1,57 @@
+import MySQLdb
+import configparser
+import sql_queries as sql
+import csv
+
+def drop_tables(cur, conn):
+	for query in sql.drop_table_queries:
+		try:
+			cur.execute(query)
+			conn.commit()
+		except:
+			break
+
+def create_tables(cur, conn):
+	for query in sql.create_table_queries:
+		try:
+			cur.execute(query)
+			conn.commit()
+		except:
+			break
+
+def load_tables(cur, conn):
+	source_data = csv.reader(open('output.csv'))
+	next(source_data)
+	for query in sql.load_table_queries:
+		for row in source_data:
+			try:
+				cur.execute(query, [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11]])
+				conn.commit()
+			except:
+				break
+
+def insert_tables(cur, conn):
+	for query in sql.insert_table_queries:
+		try:
+			cur.execute(query)
+			conn.commit()
+		except:
+			break
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+
+    conn = MySQLdb.connect(host=config['mysqldb']['host'], db=config['mysqldb']['db'], user=config['mysqldb']['user'], passwd=config['mysqldb']['pass'])
+    cur = conn.cursor()
+
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+#    load_tables(cur, conn)
+#    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
